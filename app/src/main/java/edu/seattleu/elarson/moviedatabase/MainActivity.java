c9fff546e02224129c8063c57fd2500b991a2f0e
Kelly Corrigan <kcorrigan@ah4r.com>
2017-01-29 18:35:38 -0800

    Made changes described in email from Mike based on 'lab answer'.

--- app/src/main/java/edu/seattleu/elarson/moviedatabase/MainActivity.java
+++ app/src/main/java/edu/seattleu/elarson/moviedatabase/MainActivity.java
@@ -1,12 +1,47 @@
+// Kelly Corrigan
+// SEGR 5910 - Lab 4
+// 1/29/2017
+
 package edu.seattleu.elarson.moviedatabase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    /**
+     * Inflate the menu; this adds items to the action bar if it is present.
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    /**
+     * Handle action bar item clicks here. The action bar will
+     * automatically handle clicks on the Home/Up button, so long
+     * as you specify a parent activity in AndroidManifest.xml.
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
