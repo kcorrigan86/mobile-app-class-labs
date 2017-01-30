b3f4d9421f89526cd6e49de4942e619ded97fad0
Kelly Corrigan <kcorrigan@ah4r.com>
2017-01-29 17:54:06 -0800

    Tried to implement back button in activity bar based on code from Mike's email. Doesn't work correctly

---
+++ app/src/main/java/edu/seattleu/elarson/moviedatabase/MainActivity.java
@@ -0,0 +1,12 @@
+package edu.seattleu.elarson.moviedatabase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
